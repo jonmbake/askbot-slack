c13c515a7c740d293380e50b886ff35ff67e8690
Jon Bake <jonmbake@gmail.com>
2015-08-22 13:23:33 -0500

    Add comments to method.  Clean up coding style a bit

--- askbot_slack.py
+++ askbot_slack.py
@@ -8,16 +8,25 @@ from askbot.models import Post
 
 
 def get_url(model_instance):
+    """
+    Returns the URL for the model instance.  Dependent on Site Domain name being set.
+    """
     return "http://%s%s" % (Site.objects.get_current().domain, model_instance.get_absolute_url())
 
 
 def post_msg(msg):
+    """
+    Post message to specific slack channel defined in config.
+    """
     payload = {"text": msg, "username": config.get('SLACK_POST_USERNAME'), "channel": config.get('SLACK_POST_CHANNEL')}
     requests.post(config.get('SLACK_WEBHOOK_URL'), data=json.dumps(payload))
 
 
 @receiver(post_save, sender=Post)
-def notify_question_posted(sender, instance, created, raw, using, **kwargs):
+def notify_post_created(sender, instance, created, raw, using, **kwargs):
+    """
+    Post message when Askbot Post is created.  A Post includes Questions, Comments and Answers.
+    """
     if created:
         title = instance.thread.title
         if instance.is_question():
@@ -30,4 +39,10 @@ def notify_question_posted(sender, instance, created, raw, using, **kwargs):
 
 
 class SlackMiddleware(object):
+    """
+    A NO-OP middleware class to ensure our receiver gets registered early on.
+    From the django 1.5 docs on signals: 'make sure that the module it’s in gets imported early on so that the signal
+    handling gets registered before any signals need to be sent'.  Registering as a no-op middleware class ensures that
+    this is 'imported early on'.  In later versions of django we can use the App#register method.
+    """
     pass
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name = "askbot-slack",
-    version = "0.1.1",
-     classifiers=[
+    name="askbot-slack",
+    version="0.1.2",
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 2",
@@ -11,14 +11,14 @@ setup(
         "Programming Language :: Python :: 2.7",
     ],
     py_modules=['askbot_slack'],
-    install_requires = ['askbot', 'requests'],
-    author = "Jon Bake",
-    author_email = "jonmbake@gmail.cmo",
-    description = "Simple Slack integration for Askbot.",
-    long_description =  "When questions are created, edited or responded to in Askbot a message is sent to a specified channel in Slack.",
-    license = "MIT",
-    keywords = "askbot slack integration",
-    url = "https://github.com/jonmbake/askbot-slack",
+    install_requires=['askbot', 'requests'],
+    author="Jon Bake",
+    author_email="jonmbake@gmail.com",
+    description="Simple Slack integration for Askbot.",
+    long_description="When questions are created, edited or responded to in Askbot a message is sent to a specified channel in Slack.",
+    license="MIT",
+    keywords="askbot slack integration",
+    url="https://github.com/jonmbake/askbot-slack",
     include_package_data=True,
     zip_safe=False,
 )
