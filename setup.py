8424c1c4c07a8ef4d4f734036b77a8274a99581c
Jon Bake <jonmbake@gmail.com>
2016-04-08 20:59:20 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="askbot-slack",
-    version="0.1.2",
+    version="0.1.4",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
